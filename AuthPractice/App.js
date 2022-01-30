7405ac0aae38f600c565c1aae2e51400b7c25cbb
j-knudson <71792574+j-knudson@users.noreply.github.com>
2022-01-30 17:11:55 -0600

    Add files via upload

---
+++ AuthPractice/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {useState} from "react";
+import { StyleSheet, Text, View, Image } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+
+export default function App() {
+/*  const [email, setEmail] = useState('');
+  const [password, setPassword] =  useState('');*/
+  return <WelcomeScreen />;
+}
+
+
